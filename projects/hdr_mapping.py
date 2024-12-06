f0f9c93a6f2a2c3886d413cdaf759cfcc08a1c2a
elliotl-rfpro <elliot.london@rfpro.com>
2024-12-06 16:12:30 +0000

    adjusted fog and stat plotting

--- projects/hdr_mapping.py
+++ projects/hdr_mapping.py
@@ -15,10 +15,21 @@ import cv2
 sns.set(palette="dark", font_scale=1.1, color_codes=True)
 sns.set_style('darkgrid', {'axes.linewidth': 1, 'axes.edgecolor': 'black'})
 
+
+def normalise2rgb(arr, new_min, new_max):
+    """
+    Normalize an input array between new min and max
+    """
+    old_min = np.min(arr)
+    old_max = np.max(arr)
+    normalized_arr = (new_max - new_min) * (arr - old_min) / (old_max - old_min) + new_min
+    return normalized_arr
+
+
 if __name__ == '__main__':
     # Define folders
     base_folder = 'C:/Users/ElliotLondon/Documents/PythonLocal/S4CWinterTesting/data/Fog_Comparison'
-    sim_path = f'{base_folder}/Sim_Accurate_Fog'
+    sim_path = f'{base_folder}/Sim_Trees_Removed'
     meas_path = f'{base_folder}/Meas'
 
     # Define reference ROIs
@@ -73,13 +84,13 @@ if __name__ == '__main__':
         reg2_meas.append(np.mean(meas_images[i][wall_reg_meas[1]:wall_reg_meas[3], wall_reg_meas[0]:wall_reg_meas[2]], axis=(1, 0)))
         reg3_meas.append(np.mean(meas_images[i][hill_reg_meas[1]:hill_reg_meas[3], hill_reg_meas[0]:hill_reg_meas[2]], axis=(1, 0)))
         reg4_meas.append(np.mean(meas_images[i][fog_reg_meas[1]:fog_reg_meas[3], fog_reg_meas[0]:fog_reg_meas[2]], axis=(1, 0)))
-        total_meas.append(np.mean(meas_images[i], axis=(1, 0)))
+        total_meas.append(meas_images[i])
         # Simulated
         reg1_sim.append(np.mean(sim_images[i][sky_reg_sim[1]:sky_reg_sim[3], sky_reg_sim[0]:sky_reg_sim[2]], axis=(1, 0)))
         reg2_sim.append(np.mean(sim_images[i][wall_reg_sim[1]:wall_reg_sim[3], wall_reg_sim[0]:wall_reg_sim[2]], axis=(1, 0)))
         reg3_sim.append(np.mean(sim_images[i][hill_reg_sim[1]:hill_reg_sim[3], hill_reg_sim[0]:hill_reg_sim[2]], axis=(1, 0)))
         reg4_sim.append(np.mean(sim_images[i][fog_reg_sim[1]:fog_reg_sim[3], fog_reg_sim[0]:fog_reg_sim[2]], axis=(1, 0)))
-        total_sim.append(np.mean(sim_images[i], axis=(1, 0)))
+        total_sim.append(sim_images[i])
     reg1_meas = np.array(reg1_meas)
     reg2_meas = np.array(reg2_meas)
     reg3_meas = np.array(reg3_meas)
@@ -147,21 +158,63 @@ if __name__ == '__main__':
     plt.xlabel('Time of day')
     plt.ylabel('Luminance (cd/m^2)')
     plt.legend()
-    plt.show()
 
     fig, ax = plt.subplots()
-    plt.plot(total_meas[:, 0], 'r-.', label='Measured')
-    plt.plot(total_sim[:, 0], 'r', label='Simulated')
-    plt.plot(total_meas[:, 1], 'g-.')
-    plt.plot(total_sim[:, 1], 'g')
-    plt.plot(total_meas[:, 2], 'b-.')
-    plt.plot(total_sim[:, 2], 'b')
+    plt.plot(np.mean(total_meas[:, 0], axis=1), 'r-.', label='Measured')
+    plt.plot(np.mean(total_sim[:, 0], axis=1), 'r', label='Simulated')
+    plt.plot(np.mean(total_meas[:, 1], axis=1), 'g-.')
+    plt.plot(np.mean(total_sim[:, 1], axis=1), 'g')
+    plt.plot(np.mean(total_meas[:, 2], axis=1), 'b-.')
+    plt.plot(np.mean(total_sim[:, 2], axis=1), 'b')
     plt.title('Average luminance: entire scene')
     # plt.xticks(adj_times)
+    plt.xlabel('Time of day')
+    plt.ylabel('Luminance (cd/m^2)')
+    plt.legend()
+
+    # Plot the essential statistics for shaping image colouring
+    fig, ax = plt.subplots()
+    # plt.plot(np.mean(total_meas[:, 0], axis=1), 'r-.', label='Mean R')
+    # plt.plot(np.mean(total_meas[:, 1], axis=1), 'g-.', label='Mean G')
+    # plt.plot(np.mean(total_meas[:, 2], axis=1), 'b-.', label='Mean B')
+    max_r = []
+    max_g = []
+    max_b = []
+    min_r = []
+    min_g = []
+    min_b = []
+    for i in range(total_meas.shape[0]):
+        max_r.append(np.max(total_meas[i, :, :, 0]))
+        max_g.append(np.max(total_meas[i, :, :, 1]))
+        max_b.append(np.max(total_meas[i, :, :, 2]))
+        min_r.append(np.min(total_meas[i, :, :, 0]))
+        min_g.append(np.min(total_meas[i, :, :, 1]))
+        min_b.append(np.min(total_meas[i, :, :, 2]))
+    plt.plot(max_r, 'r', label='Max R')
+    plt.plot(max_g, 'g', label='Max G')
+    plt.plot(max_b, 'b', label='Max B')
+    plt.plot(min_r, 'r', label='Min R')
+    plt.plot(min_g, 'g', label='Min G')
+    plt.plot(min_b, 'b', label='Min B')
+
+    # Ranges
+    # plt.plot(np.max(total_meas[:, :, 0], axis=1) - np.min(total_meas[:, :, 0], axis=1), 'r.-', label='Meas Range R')
+    # plt.plot(np.max(total_meas[:, :, 1], axis=1) - np.min(total_meas[:, :, 1], axis=1), 'g.-', label='Meas Range G')
+    # plt.plot(np.max(total_meas[:, :, 2], axis=1) - np.min(total_meas[:, :, 2], axis=1), 'b.-', label='Meas Range B')
+    # range1 = normalise2rgb(np.max(total_sim[:, :, 0], axis=1) - np.min(total_sim[:, :, 0], axis=1), 40, 150)
+    # range2 = normalise2rgb(np.max(total_sim[:, :, 1], axis=1) - np.min(total_sim[:, :, 1], axis=1), 40, 150)
+    # range3 = normalise2rgb(np.max(total_sim[:, :, 2], axis=1) - np.min(total_sim[:, :, 2], axis=1), 40, 150)
+    # plt.plot(range1, 'r-.', label='Sim Range R')
+    # plt.plot(range2, 'g-.', label='Sim Range G')
+    # plt.plot(range3, 'b-.', label='Sim Range B')
+
+    # Footer
+    plt.title('Luminance: entire scene')
     plt.xlabel('Time of day')
     plt.ylabel('Luminance (cd/m^2)')
     plt.legend()
     plt.show()
 
     print("Script finished successfully.")
+
 
